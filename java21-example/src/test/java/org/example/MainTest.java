c9f9e90705468d70290f88e1b1726aea11ee66ce
zhengyd <itszhengyadong@qq.com>
2023-10-19 01:22:29 +0800

    Java21-example

--- java21-example/src/test/java/org/example/MainTest.java
+++ java21-example/src/test/java/org/example/MainTest.java
@@ -11,6 +11,9 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * 全面的新特性见<a href="https://jdk.java.net/21/">官方文档</a>
+ */
 class MainTest {
 
     /**
@@ -31,21 +34,56 @@ class MainTest {
     /**
      * Record class
      */
+    record Person(String name, int age) {
+    }
+
     @Test
-    void newRecordClass(){
-        record Person(String name, int age) {}
+    void newRecordClass() {
         Person alice = new Person("Alice", 30);
         assertEquals(30, alice.age);
         assertEquals("Alice", alice.name);
-        assertEquals("Person[name=Alice, age=30]",alice.toString());
+        assertEquals("Person[name=Alice, age=30]", alice.toString());
+        check(alice);
     }
 
+    void check(Object o) {
+        if (o instanceof Person(String name, int age)) {
+            assertEquals(30, age);
+            assertEquals("Alice", name);
+            assertEquals("Person[name=Alice, age=30]", o.toString());
+        }
+    }
+
+
+    /**
+     * 增强 switch
+     */
+    @Test
+    void enhancedSwitch() {
+        Object[] objects = {"String", 1, new Person("Alice", 30)};
+        for (Object object : objects) {
+            switch (object) {
+                case String s:
+                    assertEquals("String", s);
+                    break;
+                case Integer i:
+                    assertEquals(1, i);
+                    break;
+                case Person(String name, int age):
+                    assertEquals(30, age);
+                    assertEquals("Alice", name);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + object);
+            }
+        }
+    }
 
     /**
      * 文本块
      */
     @Test
-    void textBlocks(){
+    void textBlocks() {
         String text = """
                       静夜思
                     窗前明月光，
@@ -60,7 +98,7 @@ class MainTest {
      * 新的 stream API
      */
     @Test
-    void newStreamAPI(){
+    void newStreamAPI() {
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
         long count = names.stream().filter(name -> name.startsWith("A")).count();
         assertEquals(1, count);
