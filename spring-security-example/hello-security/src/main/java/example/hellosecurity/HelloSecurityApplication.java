7604bbb93dbabbb95759b50296fe21b6904bdf12
zhengyd <itszhengyadong@qq.com>
2023-10-22 00:24:11 +0800

    hello-security

---
+++ spring-security-example/hello-security/src/main/java/example/hellosecurity/HelloSecurityApplication.java
@@ -0,0 +1,16 @@
+package example.hellosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  <a href="https://docs.spring.io/spring-security/reference/servlet/getting-started.html">README</a>
+ */
+@SpringBootApplication
+public class HelloSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSecurityApplication.class, args);
+	}
+
+}
