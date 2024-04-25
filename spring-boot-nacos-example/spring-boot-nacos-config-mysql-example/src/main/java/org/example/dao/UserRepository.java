52bd017441c031345884071b1607c85a06f03f29
郑亚东 <68491620+ItsZhengyd@users.noreply.github.com>
2024-04-25 17:14:41 +0800

    Delete spring-boot-nacos-example directory

--- spring-boot-nacos-example/spring-boot-nacos-config-mysql-example/src/main/java/org/example/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao;
-
-
-import org.example.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
